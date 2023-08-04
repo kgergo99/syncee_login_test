99911330f481ae445ae6891efce76b15d76b07ab
kgergo99 <kgergo99@mailbox.unideb.hu>
2023-08-04 10:49:13 +0200

    fetchdata.js added

---
+++ login_app/src/scripts/fetchdata.js
@@ -0,0 +1,13 @@
+const fetchCurrencies = async () => {
+    try {
+      const response = await fetch("https://api.coinbase.com/v2/currencies");
+      const data = await response.json();
+      return data.data;
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      return [];
+    }
+  };
+  
+  export default fetchCurrencies;
+  
